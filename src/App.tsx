83534ed0754e6c5a7db4c646ef1bf8d104ba64bb
Steven R. Loomis <srl295@gmail.com>
2024-04-05 17:42:41 -0500

    hack in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,58 @@
-import React from 'react';
-import ReactPDF from '@react-pdf/renderer';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Document, Page, Text, Font, usePDF, StyleSheet } from '@react-pdf/renderer'
+import ArialBold from './arial-bold.ttf'
 
-function App() {
+Font.register({
+  family: 'Arial',
+  fonts: [
+    {
+      src: ArialBold,
+      fontStyle: 'normal',
+      fontWeight: 'bold'
+    }
+  ]
+})
+
+const styles = StyleSheet.create({
+  heading: {
+    //comment these 2 lines out and it will generate PDF properly
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+
+    fontSize: 20,
+    color: '#006699'
+  }
+})
+
+const App = () => {
+  const [instance] = usePDF({
+    document: (
+      <Document title={'Title'} author={'Author'} subject={'Subject'} pageMode="useNone">
+        <Page size="A4">
+          <Text style={styles.heading}>text1</Text>
+          <Text>text2</Text>
+          <Text>text3</Text>
+        </Page>
+      </Document>
+    )
+  })
+
+  console.log('ArialBold', ArialBold)
+  console.log('Font.getRegisteredFonts().', Font.getRegisteredFonts())
+  console.log('instance', instance)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>PDF Test</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <a href={instance.url || undefined} download={'filename.pdf'}>
+      DOWNLOAD PDF
+    </a>
+  )
 }
 
-ReactPDF.render(App(), `${__dirname}/example.pdf`);
+const root = ReactDOM.createRoot(document.getElementById('root')!)
 
-export default App;
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
