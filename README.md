5e81041d36761fa4920887907b85ccd80261b4fc
Steven R. Loomis <srl295@gmail.com>
2024-04-05 18:04:37 -0500

    try again

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 - - and https://github.com/foliojs/restructure/issues/61
 
 ## this branch from <https://www.carlrippon.com/creating-react-app-with-typescript-eslint-with-webpack5/>
+
+## to repro
+
+- `npm i`
+- `npm start`
+
+view the site.. and then ?? step 3 profit
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Document, Page, Text, Font, usePDF, StyleSheet } from '@react-pdf/renderer'
+import ArialBold from './arial-bold.ttf'
 
-const App = () => (
-  <h1>My React and TypeScript App!</h1>
-);
+Font.register({
+  family: 'Arial',
+  fonts: [
+    {
+      src: ArialBold,
+      fontStyle: 'normal',
+      fontWeight: 'bold'
+    }
+  ]
+})
 
-ReactDOM.render(
+const styles = StyleSheet.create({
+  heading: {
+    //comment these 2 lines out and it will generate PDF properly
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+
+    fontSize: 20,
+    color: '#006699'
+  }
+})
+
+const App = () => {
+  const [instance] = usePDF({
+    document: (
+      <Document title={'Title'} author={'Author'} subject={'Subject'} pageMode="useNone">
+        <Page size="A4">
+          <Text style={styles.heading}>text1</Text>
+          <Text>text2</Text>
+          <Text>text3</Text>
+        </Page>
+      </Document>
+    )
+  })
+
+  console.log('ArialBold', ArialBold)
+  console.log('Font.getRegisteredFonts().', Font.getRegisteredFonts())
+  console.log('instance', instance)
+
+  return (
+    <a href={instance.url || undefined} download={'filename.pdf'}>
+      DOWNLOAD PDF
+    </a>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
